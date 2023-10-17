e05a1fc70671a5e89d669a3bf1898261d4573ed4
Abimael Sergio <abimaelsergio@rivian.com>
2023-10-16 21:47:02 -0300

    async/await

--- index.js
+++ index.js
@@ -5,22 +5,25 @@ function trataErro(erro) {
     throw new Error(chalk.red(erro.code, 'Não há arquivo no diretorio'));
 }
 
-function pegaArquivo(caminhoDoArquivo) {
+// async/await
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro);
+    }
+}
+
+// Promisses com Then
+/*function pegaArquivo(caminhoDoArquivo) {
     const encoding = 'utf-8';
     fs.promises.readFile(caminhoDoArquivo, encoding)
     .then((texto) => console.log(chalk.green(texto)))
     .catch(trataErro);
-}
-
-/*function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro) {
-            trataErro(erro);
-        }
-        console.log(chalk.greenBright(texto));
-    } );
 }*/
 
-
 pegaArquivo('./arquivos/texto.md');
+
+//pegaArquivo('./arquivos/');
